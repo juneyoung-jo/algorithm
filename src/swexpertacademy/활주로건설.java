9b2d8414809442bb37cdbd20f113aaa74b0621f1
조준영 <whwnsdud2000@naver.com>
2020-08-21 00:05:20 +0900

    8/21 00:04 PC

--- src/swexpertacademy/활주로건설.java
+++ src/swexpertacademy/활주로건설.java
@@ -21,7 +21,6 @@ public class 활주로건설 {
 			X = Integer.parseInt(st.nextToken());
 
 			map = new int[N][N];
-			v = new boolean[N]; // 중복검사
 
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
@@ -32,10 +31,107 @@ public class 활주로건설 {
 
 			// 알고리즘
 			// 행 열 나눠서 계산
-
 			// 행
+			int cnt = 0;
+
+			for (int i = 0; i < map.length; i++) {
+				boolean isOk = false;
+				v = new boolean[N]; // 중복검사
+				for (int j = 0; j < map[i].length - 1; j++) {
+					// 길이가 2이상 차이나면 다음 행or 열로 넘어감
+					if (map[i][j] - map[i][j + 1] > 1 || map[i][j] - map[i][j + 1] < -1) {
+						break;
+					}
+					// 길이가 1차이일 때
+					if (map[i][j] - map[i][j + 1] == 1) {
+						for (int k = 1; k <= X; k++) {
+							if (j + k >= N) {
+								isOk = true;
+								break;
+							}
+							// 이미 활주로가 놓여져있으면 끝내기
+							if (v[j + k]) {
+								isOk = true;
+								break;
+							}
+							v[j + k] = true;
+						}
+
+					}
+
+					// 길이가 -1차이일 때
+					if (map[i][j] - map[i][j + 1] == -1) {
+						for (int k = 0; k < X; k++) {
+							if (j - k < 0) {
+								isOk = true;
+								break;
+							}
+							if (v[j - k]) {
+								isOk = true;
+								break;
+							}
+							v[j - k] = true;
+						}
+
+					}
+
+					if (isOk) {
+						break;
+					}
+					if (j == N - 2) {
+						cnt++;
+					}
+				}
+			}
 
 			// 열
+			for (int i = 0; i < map.length; i++) {
+				boolean isOk = false;
+				v = new boolean[N]; // 중복검사
+				for (int j = 0; j < map[i].length - 1; j++) {
+					if (map[j][i] - map[j + 1][i] > 1 || map[j][i] - map[j + 1][i] < -1) {
+						break;
+					}
+					if (map[j][i] - map[j + 1][i] == 1) {
+						for (int k = 1; k <= X; k++) {
+							if (j + k >= N) {
+								isOk = true;
+								break;
+							}
+							if (v[j + k]) {
+								isOk = true;
+								break;
+							}
+							v[j + k] = true;
+						}
+
+					}
+
+					if (map[j][i] - map[j + 1][i] == -1) {
+						for (int k = 0; k < X; k++) {
+							if (j - k < 0) {
+								isOk = true;
+								break;
+							}
+							if (v[j - k]) {
+								isOk = true;
+								break;
+							}
+							v[j - k] = true;
+						}
+
+					}
+
+					if (isOk) {
+						break;
+					}
+					if (j == N - 2) {
+						cnt++;
+					}
+				}
+			}
+
+			System.out.printf("#%d %d\n", tc, cnt);
 
 		}
 
