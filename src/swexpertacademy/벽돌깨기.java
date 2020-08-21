4355c6d99923bb0f10225cde4e3c80fc84ae3d1e
multicampus <multicampus@M805>
2020-08-21 16:56:03 +0900

    8/21 16:55 멀캠

--- src/swexpertacademy/벽돌깨기.java
+++ src/swexpertacademy/벽돌깨기.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class 벽돌깨기 {
-	static int T, N, W, H, map[][], arr[], Ans; // H x W 배열
+	static int T, N, W, H, map[][], arr[], Ans = Integer.MAX_VALUE, copymap[][]; // H x W 배열
 	static boolean v[][];
 	static int[] dr = { 1, -1, 0, 0 };
 	static int[] dc = { 0, 0, 1, -1 };
@@ -49,60 +49,89 @@ public class 벽돌깨기 {
 			// 중복 순열만큼 dfs를 돌리는데 한번 돌리고 다 깨지면 깨진 블록 다시 배치해야함.
 
 			arr = new int[N]; // 중복순열 값 담는 배열
-//			permutaiton(0);
-			v = new boolean[H][W];
-			dfs(new Point(2, 2, 1));
-			newmap(map);
+			permutaiton(0); // 중복 순열하면서 dfs안에서 돌려야함.
+			if (Ans == Integer.MAX_VALUE) { // 값이 안 바뀔 때
+				Ans = 0;
+			}
 
-			print(map);
+			System.out.printf("#%d %d\n", tc, Ans);
+			Ans = Integer.MAX_VALUE;
 
-			System.out.println(Ans);
 		}
 
 	}
 
-	private static void newmap(int[][] map) {
-		for (int i = 0; i < map.length; i++) {
-			for (int j = 0; j < map[i].length; j++) {
+	private static void sortmap(int[][] map) {// dfs돌 때 마다 사라진 블록을 제외하고 맵을 정렬해줘야 함..
+		for (int i = 0; i < W; i++) {
+			int arr[] = new int[H]; // 정렬할 때 배열에 저장해서 다 출력하는 식으로 만듬
+			int cnt = 0;
+			for (int j = H - 1; j >= 0; j--) {// 끝에서부터 값이 있으면 배열에 저장
+				if (map[j][i] != 0) {
+					arr[cnt++] = map[j][i];
 
+				}
+			}
+			for (int j = 0; j < H; j++) {
+				map[H - j - 1][i] = arr[j];
 			}
 		}
-
 	}
 
 	private static void permutaiton(int idx) {
 		if (idx == N) {
-//			System.out.println(Arrays.toString(arr));
+			copymap = new int[H][W]; // 새로운 맵
+			cmap(copymap);// 매번 새로운 맵 만들어 줘야 함.
 			for (int i = 0; i < arr.length; i++) {
-				if (search(arr[i]) == 0) {
+				if (search(arr[i]) == -1) { // 만약 col에 더 부실 벽돌이 없다면 더 할 필요없이 리턴
 					return;
 				}
-				v = new boolean[H][W];
-				dfs(new Point(search(arr[i]), arr[i], 1));
-
+				v = new boolean[H][W]; // 매번 탐색확인 배열 초기화해줘야함
+				dfs(new Point(search(arr[i]), arr[i], 1), copymap); // dfs돌리면서 copymap 같이 돌리기
+				sortmap(copymap); // 정렬
 			}
+			Ans = Math.min(Ans, countmap(copymap)); // 최저값 저장
 			return;
 		}
 
-		for (int i = 0; i < W; i++) {
+		for (int i = 0; i < W; i++) { // 중복순열
 			arr[idx] = i;
 			permutaiton(idx + 1);
 		}
 
 	}
 
-	private static void dfs(Point p) {
+	private static int countmap(int[][] copymap) {// 다 끝나고 남은 블록 세주는 함수
+		int cnt = 0;
+		for (int i = 0; i < copymap.length; i++) {
+			for (int j = 0; j < copymap[i].length; j++) {
+				if (copymap[i][j] != 0) {
+					cnt++;
+				}
+			}
+		}
+		return cnt;
+	}
+
+	private static void cmap(int[][] copymap) {// 모든 경우를 다 돌려야하기 때문에 맵을 계속 새로만들어줘야함.
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < copymap[i].length; j++) {
+				copymap[i][j] = map[i][j];
+			}
+		}
+	}
+
+	private static void dfs(Point p, int[][] copymap) { // 깨진블록들 0으로 바꿔주면서 dfs돌림
 		v[p.r][p.c] = true;
-		int size = map[p.r][p.c];
-		map[p.r][p.c] = 0;
+		int size = copymap[p.r][p.c];
+		copymap[p.r][p.c] = 0;
 
 		for (int k = 0; k < 4; k++) {
 			for (int cn = 0; cn < size; cn++) {
 				int nr = p.r + dr[k] * cn;
 				int nc = p.c + dc[k] * cn;
 
-				if (nr >= 0 && nc >= 0 && nr < H && nc < W && map[nr][nc] != 0 && !v[nr][nc]) {
-					dfs(new Point(nr, nc, p.cnt++));
+				if (nr >= 0 && nc >= 0 && nr < H && nc < W && copymap[nr][nc] != 0 && !v[nr][nc]) {
+					dfs(new Point(nr, nc, p.cnt++), copymap);
 				}
 			}
 
@@ -111,13 +140,13 @@ public class 벽돌깨기 {
 	}
 
 	private static int search(int i) { // dfs 시작할 행을 구함. 열은 i값
-		for (int r = 0; r < map.length; r++) {
-			if (map[r][i] != 0) {
+		for (int r = 0; r < copymap.length; r++) {
+			if (copymap[r][i] != 0) {
 				return r;
 			}
 		}
 
-		return 0;
+		return -1;
 	}
 
 	private static void print(int[][] map) {
---
+++ src/swexpertacademy/줄기세포배양.java
@@ -0,0 +1,126 @@
+package swexpertacademy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class 줄기세포배양 {
+	static int T, N, M, K, map[][], nmap[][][], Ans;
+	static boolean[][] v;
+	static int[] dr = { -1, 1, 0, 0 };
+	static int[] dc = { 0, 0, 1, -1 };
+
+	static class Point implements Comparable<Point> {
+		int r;
+		int c;
+		int cnt;
+
+		public Point(int r, int c, int cnt) {
+			super();
+			this.r = r;
+			this.c = c;
+			this.cnt = cnt;
+		}
+
+		@Override
+		public int compareTo(Point o) {
+			return Integer.compare(o.cnt, this.cnt);
+		}
+
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
+
+			map = new int[N][M];
+			nmap = new int[N + K][M + K][3]; // 상하좌우로 K값만큼만 크면됨(증식 최대사이즈). newMap의 중앙에 map값 저장해야함
+			v = new boolean[N + K][M + K]; // 중복체크
+
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < M; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+					// 여기에 nmap도 같이 초기화
+					nmap[K / 2 + i][K / 2 + j][0] = map[i][j]; // 현재 cnt값 -> 증식한 칸에 cnt값 표현하기 위함.
+					nmap[K / 2 + i][K / 2 + j][1] = map[i][j]; // Q에 넣는 기준이 됨 -> cnt값을 1개씩 -1일 경우에 Q에 add함
+					nmap[K / 2 + i][K / 2 + j][2] = map[i][j]; // 정답이 되는 기준 -> 증식한 다음에도 x시간 동안 살아있기 때문에
+				}
+			}
+
+			// 알고리즘
+			// 1. 새로운 맵을 만들어 기존맵을 중앙에 배치 ㅇㅇ
+			// 2. bfs를 사용하는데 Point 객체 만들어 r,c, cnt값 사용, 우선순위큐 사용해야할듯 값이 큰거 먼저 돌려야함
+			// 3. 시간이 지남에 따라 값이 변경되기 때문에 3중포문(시간, 행, 열)
+			PriorityQueue<Point> q = new PriorityQueue<Point>();
+
+			for (int t = 0; t < K; t++) {
+				for (int r = 0; r < nmap.length; r++) {
+					for (int c = 0; c < nmap[r].length; c++) {
+						nmap[r][c][1] -= 1;
+
+						if (nmap[r][c][0] != 0 && !v[r][c] && nmap[r][c][1] == -1) {
+							nmap[r][c][2] -= 1; // 증식 할 때 -1해줘야함.(안 하면 1 높아서 값 틀림)
+							q.add(new Point(r, c, nmap[r][c][0]));
+						}
+						if (v[r][c]) {
+							nmap[r][c][2] -= 1; // 증식한 이후에 하나씩 빼줘서 살아있는지 죽었는지 확인함
+						}
+					}
+				}
+				while (!q.isEmpty()) {
+					dfs(q.poll()); // 우선순위 큐를 사용한 dfs => 깊이가 1임.
+				}
+			}
+
+			cntmap(nmap);
+			System.out.printf("#%d %d\n", tc, Ans);
+			Ans = 0;
+
+		}
+
+	}
+
+	private static void cntmap(int[][][] nmap) {
+		for (int r = 0; r < nmap.length; r++) {
+			for (int c = 0; c < nmap[r].length; c++) {
+				if (nmap[r][c][2] > 0) {
+					Ans++;
+				}
+			}
+		}
+	}
+
+	private static void dfs(Point p) {
+		v[p.r][p.c] = true;
+
+		for (int k = 0; k < 4; k++) {
+			int nr = p.r + dr[k];
+			int nc = p.c + dc[k];
+
+			if (nr >= 0 && nc >= 0 && nr < N + K && nc < M + K && !v[nr][nc] && nmap[nr][nc][0] == 0) {
+				nmap[nr][nc][0] = p.cnt;
+				nmap[nr][nc][1] = p.cnt;
+				nmap[nr][nc][2] = p.cnt;
+			}
+		}
+	}
+
+	private static void print(int[][][] map) {
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[i].length; j++) {
+				System.out.print(map[i][j][2] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
