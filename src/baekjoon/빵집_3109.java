43017be5013990ff9a9cd208a012ff30c713d134
조준영 <whwnsdud2000@naver.com>
2020-08-30 16:00:02 +0900

    pc

--- src/baekjoon/빵집_3109.java
+++ src/baekjoon/빵집_3109.java
@@ -8,10 +8,10 @@ import java.util.StringTokenizer;
 public class 빵집_3109 {
 	static int R, C, Ans;
 	static char[][] map;
-	static boolean[][] v;
 	static int[] dr = { -1, 0, 1 };
 	static int[] dc = { 1, 1, 1 };
-	static boolean vf;
+	static boolean[][] v;
+	static boolean flag;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,48 +27,41 @@ public class 빵집_3109 {
 		}
 
 		for (int r = 0; r < R; r++) {
-			vf = true;
-			v[r][0] = true;
+			flag = true;
 			dfs(r, 0);
 		}
 		System.out.println(Ans);
+//		print(v);
+	}
 
-//		print(map);
-
+	private static void print(boolean[][] v) {
+		for (int i = 0; i < v.length; i++) {
+			for (int j = 0; j < v[i].length; j++) {
+				System.out.print(v[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 
 	private static void dfs(int r, int c) {
 		if (c == C - 1) {
+			flag = false;
 			Ans++;
-			// 파이프가 연결이 되면 그 재귀모두 종료해야함.
-			// 연결이 되는지 안 되는지가 중요함 모든경우가 필요한게 아님
-			vf = false;
 			return;
 		}
 
 		for (int k = 0; k < 3; k++) {
-
 			int nr = r + dr[k];
 			int nc = c + dc[k];
 
-			if (nr < 0 || nr >= R || v[nr][nc] || map[nr][nc] == 'x')
+			if (nr < 0 || nc < 0 || nr >= R || nc >= C)
 				continue;
-			if (!vf)
-				return;
+			if (v[nr][nc] || map[nr][nc] == 'x' || !flag)
+				continue;
 			v[nr][nc] = true;
 			dfs(nr, nc);
 		}
-		return;
-	}
 
-	private static void print(char[][] map) {
-		// TODO Auto-generated method stub
-		for (int i = 0; i < map.length; i++) {
-			for (int j = 0; j < map[i].length; j++) {
-				System.out.print(map[i][j] + " ");
-			}
-			System.out.println();
-		}
 	}
 
 }
--- src/swexpertacademy/최대상금.java
+++ src/swexpertacademy/최대상금.java
@@ -3,13 +3,12 @@ package swexpertacademy;
 import java.util.Arrays;
 import java.util.Scanner;
 
-
 public class 최대상금 {
-	static int T, arr[], N;
+	static int T, N;
+	static Integer[] arr;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();
 
 		T = sc.nextInt();
 
@@ -17,35 +16,59 @@ public class 최대상금 {
 			String str = sc.next();
 			N = sc.nextInt();
 
-			arr = new int[str.length()];
+			arr = new Integer[str.length()];
 			// 값 담기
 			Integer[] comp = new Integer[str.length()];
 			for (int i = 0; i < str.length(); i++) {
 				arr[i] = str.charAt(i) - '0';
 				comp[i] = str.charAt(i) - '0';
 			}
-			int num = Integer.parseInt(str);
 
 			// 역순 정렬 (가장 큰 숫자만들기)
-			Arrays.sort(comp, (a, b) -> {
-				return b - a;
-			});
-			for (int comps : comp) {
-				sb.append(comps);
-			}
-			int max = Integer.parseInt(sb.toString());
+			Arrays.sort(comp, (a, b) -> b - a);
 
 			// n번 횟수만큼
-			int start = 0;
-			while (N-- > 0) {
-				if (num == max) {
+			while (N > 0) {
+				if (Arrays.deepEquals(arr, comp)) {
 					break;
 				}
 
+				L: for (int i = 0; i < arr.length; i++) {
+					if (arr[i] != comp[i]) {
+						for (int j = arr.length - 1; j >= 0; j--) {
+							if (comp[i] == arr[j]) {
+								if (i != 0 && comp[i] == comp[i - 1])
+									N++;
+								int tmp = arr[i];
+								arr[i] = arr[j];
+								arr[j] = tmp;
+								break L;
+							}
+						}
+					}
+				}
+
+				N--;
+			}
+
+			L: while (N-- % 2 == 1) {
+				for (int i = 1; i < arr.length; i++) {
+					if (arr[i] == arr[i - 1]) {
+						break L;
+					}
+				}
+
+				int tmp = arr[arr.length - 2];
+				arr[arr.length - 2] = arr[arr.length - 1];
+				arr[arr.length - 1] = tmp;
 
 			}
 
-			System.out.println(Arrays.toString(arr) + " " + N);
+			System.out.print("#" + tc + " ");
+			for (int i = 0; i < arr.length; i++) {
+				System.out.print(arr[i]);
+			}
+			System.out.println();
 
 		}
 
