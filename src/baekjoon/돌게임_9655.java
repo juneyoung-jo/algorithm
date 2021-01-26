1dac34c4c800471eb1f36caa6441056d99582078
조준영 <whwnsdud2000@naver.com>
2021-01-27 01:20:28 +0900

    1/26 algo

---
+++ src/baekjoon/돌게임_9655.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class 돌게임_9655 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n =sc.nextInt();
+		System.out.println(n%2 == 1 ? "SK":"CY");
+	}
+
+}
