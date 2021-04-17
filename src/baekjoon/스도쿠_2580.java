69c8199deb97f05a8771160775521407dac41709
조준영 <whwnsdud2000@naver.com>
2021-04-17 23:59:02 +0900

    4/17 algo

---
+++ src/baekjoon/스도쿠_2580.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.util.*;
+import java.io.*;
+
+public class 스도쿠_2580 {
+	static int map[][];
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		map = new int[9][9];
+
+		for (int i = 0; i < 9; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			for (int j = 0; j < 9; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		
+
+	}
+
+}
