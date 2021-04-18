e42adfd608dff7d070a0ab12b43d16c6e1a6e001
조준영 <whwnsdud2000@naver.com>
2021-04-19 01:56:33 +0900

    4/19 algo

--- src/baekjoon/스도쿠_2580.java
+++ src/baekjoon/스도쿠_2580.java
@@ -18,11 +18,59 @@ public class 스도쿠_2580 {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
-		cal()
-		
-		
 
+		cal(0);
+
+	}
+
+	private static void cal(int idx) {
+		if (idx == 81) {
+			for (int i = 0; i < map.length; i++) {
+				for (int j = 0; j < map[i].length; j++) {
+					if (j == 0) System.out.print(map[i][j]);
+					else System.out.print(" " + map[i][j]);
+				}
+				System.out.println();
+			}
+			System.exit(0);
+		}
+
+		int row = idx / 9;
+		int col = idx % 9;
+
+		if (map[row][col] == 0) {
+			for (int i = 1; i <= 9; i++) {
+				if (check(row, col, i)) {
+					map[row][col] = i;
+					cal(idx + 1);
+					map[row][col] = 0;
+				}
+			}
+		} else {
+			cal(idx + 1);
+			}
+	}
+
+	private static boolean check(int row, int col, int i) {
+
+		// 행 열 체크
+		for (int j = 0; j < 9; j++) {
+			if (map[row][j] == i) return false;
+			if (map[j][col] == i) return false;
+		}
+
+
+		// 3*3 체크
+		int r = row / 3 * 3;
+		int c = col / 3 * 3;
+
+		for (int j = 0; j < 3; j++) {
+			for (int k = 0; k < 3; k++) {
+				if (map[r + j][c + k] == i) return false;
+			}
+		}
+
+		return true;
 	}
 
 }
