cb87be4f1b7ff6be83c2f61fa7449468108d3c04
whwns <whwnsdud2000@naver.com>
2021-06-25 18:45:38 +0900

    6/25 algo

---
+++ src/programmers/보석쇼핑.java
@@ -0,0 +1,59 @@
+package programmers;
+
+import java.util.*;
+
+public class 보석쇼핑 {
+	static int arr[], v[];
+	static Set<String> set = new HashSet<>();
+	static Map<String, Integer> map = new HashMap<>();
+
+	public static void main(String[] args) {
+		String[] gems = { "XYZ", "XYZ", "XYZ" };
+		System.out.println(Arrays.toString(solution(gems)));
+	}
+
+	public static int[] solution(String[] gems) {
+
+		  
+        int size = 0;
+        arr = new int[gems.length];
+		
+        for (String str : gems) set.add(str);
+		for (String str : set) map.put(str, size++);
+		for (int i = 0; i < gems.length; i++) arr[i] = map.get(gems[i]);
+        
+        v = new int[size];
+		
+		int vSize = 0;
+		int ansSize = gems.length;
+		int right = arr.length - 1;
+		int a_left = 0;
+		int a_right = arr.length - 1;
+
+		for (int i = arr.length - 1; i >= 0; i--) {
+          
+			if (v[arr[i]] == 0) vSize++;
+			v[arr[i]]++;
+          
+			if (vSize == size) {
+				while (i <= right) {
+					if (v[arr[right]] >= 2) {
+						v[arr[right]]--;
+						right--;
+					} else  break;
+				}
+				
+				if (ansSize >= right - i + 1) {
+					a_left = i;
+					a_right = right;
+					ansSize = right - i + 1;
+				}
+
+			}
+		}
+
+		int[] answer = { a_left + 1, a_right + 1 };
+		return answer;
+	}
+
+}
