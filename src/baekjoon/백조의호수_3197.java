f9533bff5f69c93a55a7c291f186bc6bd00b8fd2
young <whwnsdud2000@naver.com>
2021-07-21 01:06:24 +0900

    7/21 algo ref

--- src/baekjoon/백조의호수_3197.java
+++ src/baekjoon/백조의호수_3197.java
@@ -7,13 +7,11 @@ public class 백조의호수_3197 {
     static int[] dr = {1, -1, 0, 0};
     static int[] dc = {0, 0, 1, -1};
     static char[][] map;
-    static boolean[][] iceV;
-    static boolean[][] swanV;
+    static boolean[][] v;
     static boolean check;
     static Queue<Point> q = new LinkedList<>();
-    static Queue<Point> holdQ = new LinkedList<>();
+    static Queue<Point> iceQ = new LinkedList<>();
     static Queue<Point> swanQ = new LinkedList<>();
-    static Queue<Point> holdSwan = new LinkedList<>();
     static class Point {
         int r,c;
 
@@ -29,20 +27,18 @@ public class 백조의호수_3197 {
         C = Integer.parseInt(st.nextToken());
 
         map = new char[R][C];
-        iceV = new boolean[R][C];
-        swanV = new boolean[R][C];
+        v = new boolean[R][C];
 
         for(int i =0 ; i < R; i++) {
             String str = br.readLine();
             for(int j = 0; j < C; j++) {
                 map[i][j] = str.charAt(j);
                 if (map[i][j] == '.' || map[i][j] == 'L') {
-                    iceV[i][j] = true;
-                    holdQ.add(new Point(i,j));
+                    iceQ.add(new Point(i,j));
                 }
-                if (map[i][j] == 'L' && holdSwan.size() == 0) {
-                    swanV[i][j] = true;
-                    holdSwan.add(new Point(i,j));
+                if (map[i][j] == 'L' && swanQ.size() == 0) {
+                    v[i][j] = true;
+                    swanQ.add(new Point(i,j));
                 }
                 if (map[i][j] == 'L' ) {
                     resultR = i;
@@ -56,17 +52,15 @@ public class 백조의호수_3197 {
             swanGo();
             if(check) break;
             ans++;
-            iceGo();
+            melt();
         }
 
         System.out.println(ans);
 
     }
 
-    public static void iceGo() {
-        q.clear();
-        q.addAll(holdQ);
-        holdQ.clear();
+    public static void melt() {
+        while(!iceQ.isEmpty()) q.add(iceQ.poll());
 
         Point p  = null;
         while(!q.isEmpty()) {
@@ -77,11 +71,10 @@ public class 백조의호수_3197 {
                 int nr = p.r + dr[k];
                 int nc = p.c + dc[k];
 
-                if(nr < 0 || nc < 0 || nr >= R || nc >= C || iceV[nr][nc]) continue;
+                if(nr < 0 || nc < 0 || nr >= R || nc >= C) continue;
                 if(map[nr][nc] == 'X') {
-                    iceV[nr][nc] = true;
                     map[nr][nc]  = '.';
-                    holdQ.add(new Point(nr,nc));
+                    iceQ.add(new Point(nr,nc));
                 }
 
             }
@@ -90,26 +83,24 @@ public class 백조의호수_3197 {
     }
 
     public static void swanGo() {
-        swanQ.clear();
-        swanQ.addAll(holdSwan);
-        holdSwan.clear();
+        while(!swanQ.isEmpty()) q.add(swanQ.poll());
 
         Point p = null;
-        while(!swanQ.isEmpty()) {
-            p = swanQ.poll();
+        while(!q.isEmpty()) {
+            p = q.poll();
 
             for(int k = 0; k < 4; k++) {
                 int nr = p.r + dr[k];
                 int nc = p.c + dc[k];
 
-                if(nr < 0 || nc < 0 || nr >= R || nc >= C || swanV[nr][nc]) continue;
+                if(nr < 0 || nc < 0 || nr >= R || nc >= C || v[nr][nc]) continue;
 
                 if(map[nr][nc] == '.') {
-                    swanV[nr][nc] = true;
-                    swanQ.add(new Point(nr,nc));
+                    v[nr][nc] = true;
+                    q.add(new Point(nr,nc));
                 }else if(map[nr][nc] == 'X') {
-                    swanV[nr][nc] =true;
-                    holdSwan.add(new Point(nr,nc));
+                    v[nr][nc] =true;
+                    swanQ.add(new Point(nr,nc));
                 }else if(nr == resultR && nc == resultC) {
                     check = true;
                     return;
