3342f6222faf6124be222064f064af943f2b03c0
young <whwnsdud2000@naver.com>
2021-07-22 00:07:46 +0900

    7/22 algo

---
+++ src/programmers/광고삽입_2021KAKAO_BLIND_REC.java
@@ -0,0 +1,60 @@
+package programmers;
+import java.util.*;
+
+public class 광고삽입_2021KAKAO_BLIND_REC {
+    static long time[];
+
+    public String solution(String play_time, String adv_time, String[] logs) {
+        int playTime = formatTime(play_time);
+        time = new long[playTime+1];
+
+        for(String log : logs) {
+            String[] logTime = log.split("-");
+            int start = formatTime(logTime[0]);
+            int end = formatTime(logTime[1]);
+            time[start]++;
+            time[end]--;
+        }
+
+        // 개수 초기화
+        for(int i = 1; i < time.length; i++) {
+            time[i] += time[i-1];
+        }
+
+        // 누적합 만들기
+        for(int i = 1; i < time.length; i++) {
+            time[i] += time[i-1];
+        }
+
+        int adv = formatTime(adv_time);
+
+        long max = time[adv-1];
+        int resultTime = 0;
+        for(int i = adv; i < time.length; i++) {
+            if(time[i] - time[i-adv] > max) {
+                max = time[i] - time[i-adv];
+                resultTime = i - adv +1;
+            }
+        }
+
+        return calTime(resultTime);
+    }
+
+    public String calTime(int time) {
+        String h = String.valueOf(time/3600);
+        String m = String.valueOf(time%3600/60);
+        String s = String.valueOf(time%60);
+        if(h.length() == 1) h = "0" + h;
+        if(m.length() == 1) m = "0" + m;
+        if(s.length() == 1) s = "0" + s;
+        return h + ":" + m + ":" + s;
+    }
+
+    public static int formatTime(String time) {
+        StringTokenizer st = new StringTokenizer(time,":");
+        int h = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+        return h * 3600 + m * 60 + s;
+    }
+}
---
+++ src/programmers/순위추천_2021KAKAO_BLIND_REC.java
@@ -0,0 +1,74 @@
+package programmers;
+import java.util.*;
+
+public class 순위추천_2021KAKAO_BLIND_REC {
+    static Map<String,List<Integer>> map = new HashMap<>();
+    static String[] language = {"-","cpp", "java", "python"};
+    static String[] job = {"-","backend","frontend"};
+    static String[] career = {"-","junior","senior"};
+    static String[] food = {"-","chicken","pizza"};
+    static int[] answer;
+    public int[] solution(String[] info, String[] query) {
+        answer = new int[query.length];
+        genKey();
+        StringTokenizer st = null;
+
+        for(String inf : info) {
+            String[] infItem = inf.split(" ");
+            map.get(infItem[0] + infItem[1] + infItem[2] + infItem[3])
+                    .add(Integer.parseInt(infItem[4]));
+        }
+
+        map.forEach((key,value) -> Collections.sort(value));
+
+        int index = 0;
+        for(String q : query) {
+            String[] queryItem = q.split("and");
+            st = new StringTokenizer(queryItem[3]," ");
+
+            queryItem[0] = queryItem[0].trim();
+            queryItem[1] = queryItem[1].trim();
+            queryItem[2] = queryItem[2].trim();
+            queryItem[3] = st.nextToken();
+            addValue(queryItem,Integer.parseInt(st.nextToken()),0,"",index);
+            index++;
+        }
+
+        return answer;
+    }
+
+    public void addValue(String[] queryItem, int score, int idx, String q, int index){
+        if(idx == 4) {
+            List<Integer> list = map.get(q);
+            int size = map.get(q).size();
+            int start = 0;
+            int end = size;
+            while(start < end) {
+                int mid = (start+end)/2;
+                if(list.get(mid) < score) start = mid + 1;
+                else end = mid;
+            }
+            answer[index] += size - start;
+            return;
+        }
+
+        if(!queryItem[idx].equals("-")) addValue(queryItem, score, idx+1, q + queryItem[idx],index);
+        else{
+            for(int i = 1; i < 4; i++) {
+                if(i == 3 && idx != 0) continue;
+                if(idx == 0) addValue(queryItem, score, idx+1, q + language[i],index);
+                else if(idx == 1) addValue(queryItem, score, idx+1, q + job[i],index);
+                else if(idx == 2) addValue(queryItem, score, idx+1, q + career[i],index);
+                else if(idx == 3) addValue(queryItem, score, idx+1, q + food[i],index);
+            }
+        }
+    }
+
+    public void genKey(){
+        for(int i = 0; i < 4; i++)
+            for(int j = 0; j < 3; j++)
+                for(int k = 0; k < 3; k++)
+                    for(int l = 0; l < 3; l++)
+                        map.put(language[i] + job[j] + career[k] + food[l],new ArrayList<>());
+    }
+}
