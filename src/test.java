a449de65773f4e060913df2f93ef047ac45e00f4
young <whwnsdud2000@naver.com>
2021-07-25 23:54:42 +0900

    Test : StringBuilder ans String Test

--- src/test.java
+++ src/test.java
@@ -1,3 +1,4 @@
+import java.sql.Array;
 import java.util.*;
 
 public class test {
@@ -27,6 +28,34 @@ public class test {
         }
     }
     public static void main(String[] args) {
+
+        List<Point> list = new ArrayList<>();
+
+        list.add(new Point(10,10));
+        list.add(new Point(20,20));
+        list.add(new Point(30,30));
+        list.add(new Point(40,40));
+        list.add(new Point(50,50));
+
+        Point p = new Point(100,100);
+        for (Point point : list) {
+            point.setC(100);
+        }
+
+        for (Point point : list) {
+            System.out.println(point);
+        }
+
+        String str = "a";
+        StringBuilder sb = new StringBuilder();
+        sb.append("a");
+
+        for(int i = 0; i<1000000; i++) {
+//            str+= "a";
+            sb.append("a");
+        }
+        System.out.println(sb.toString());
+
 
     }
 
