0d33320b49d080bb5f2ff4652d6ad2de65d576bb
young <whwnsdud2000@naver.com>
2021-08-11 23:12:13 +0900

    8/11 algo programmers

---
+++ src/programmers/튜플_2019KAKAO_Winter.java
@@ -0,0 +1,26 @@
+package programmers;
+
+import java.util.*;
+
+public class 튜플_2019KAKAO_Winter {
+    static Map<String, Integer> map = new HashMap<>();
+
+    public int[] solution(String s) {
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == '{') {
+                int idx = i;
+                while (s.charAt(idx) != '}') idx++;
+                Arrays.stream(s.substring(i + 1, idx).split(","))
+                        .forEach(o -> map.merge(o, 1, Integer::sum));
+                i = idx;
+            }
+        }
+
+        return map.keySet().stream()
+                .sorted((o1, o2) -> map.get(o2) - map.get(o1))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+    }
+}
