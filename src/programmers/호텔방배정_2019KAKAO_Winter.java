88085ed34daabee90c77d879cd1f5dda6a78040d
young <whwnsdud2000@naver.com>
2021-08-11 23:13:51 +0900

    8/11 algo programmers

---
+++ src/programmers/호텔방배정_2019KAKAO_Winter.java
@@ -0,0 +1,27 @@
+package programmers;
+
+import java.util.*;
+
+public class 호텔방배정_2019KAKAO_Winter {
+    static Map<Long, Long> map = new HashMap<>();
+
+    public long[] solution(long k, long[] room_number) {
+        long[] answer = new long[room_number.length];
+
+        for (int i = 0; i < room_number.length; i++) {
+            answer[i] = cal(room_number[i]);
+        }
+        return answer;
+    }
+
+    public static long cal(long num) {
+        if (!map.containsKey(num)) {
+            map.put(num, num + 1);
+            return num;
+        }
+
+        long findNum = cal(map.get(num));
+        map.put(num, findNum + 1);
+        return findNum;
+    }
+}
