87ede6ede5671dbc1d2c1474446f00132d8aedf7
Young <whwnsdud2000@naver.com>
2021-10-13 23:17:30 +0900

    10/13 algo boj

---
+++ src/baekjoon/단절점과단절선_14675.kt
@@ -0,0 +1,25 @@
+package baekjoon
+
+import java.lang.StringBuilder
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+    val cnt = IntArray(n + 1)
+    val sb = StringBuilder()
+
+    for (i in 1 until n) {
+        val (n, m) = readLine().split(' ').map { it.toInt() }
+        cnt[n]++
+        cnt[m]++
+    }
+
+    for (i in 1..readLine().toInt()) {
+        val (n, m) = readLine().split(' ').map { it.toInt() }
+        when (n) {
+            1 -> if (cnt[m] == 1) sb.append("no\n") else sb.append("yes\n")
+            2 -> sb.append("yes\n")
+        }
+    }
+
+    println(sb)
+}
