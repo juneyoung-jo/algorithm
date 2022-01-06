3be65024533735b360d6280ae1d6165b6ec361f8
Young <juneyoung-jo@github.com>
2022-01-06 23:22:39 +0900

    01/06 algo boj

--- src/baekjoon/7453_합이0인네정수.kt
+++ src/baekjoon/7453_합이0인네정수.kt
@@ -6,10 +6,10 @@ fun main() = with(System.`in`.bufferedReader()) {
     val bList = mutableListOf<Long>()
     val cList = mutableListOf<Long>()
     val dList = mutableListOf<Long>()
-    val adSumList = mutableListOf<Long>()
-    val cdSumList = mutableListOf<Long>()
+    val abSumList = LongArray(n * n)
+    val cdSumList = LongArray(n * n)
 
-    (0 until n).forEach {
+    (0 until n).forEach { _ ->
         val temp = readLine().split(' ').map { it.toLong() }
         aList += temp[0]
         bList += temp[1]
@@ -17,16 +17,54 @@ fun main() = with(System.`in`.bufferedReader()) {
         dList += temp[3]
     }
 
-
+    var idx = 0
     for (i in 0 until n) {
         for (j in 0 until n) {
-            adSumList += aList[i] + bList[j]
-            cdSumList += cList[i] + dList[j]
+            abSumList[idx] += aList[i] + bList[j]
+            cdSumList[idx] += cList[i] + dList[j]
+            idx++
         }
     }
 
-    adSumList.sort()
-    cdSumList.sortByDescending { it }
+    abSumList.sort()
+    cdSumList.sortDescending()
 
+    var abIdx = 0
+    var cdIdx = 0
+    var result: Long = 0
+    var sumVal: Long
+    val size = abSumList.size
+    while (true) {
+        if (abIdx == size || cdIdx == size) break
+        sumVal = abSumList[abIdx] + cdSumList[cdIdx]
+        when {
+            sumVal == 0L -> {
+                var abCnt: Long = 1
+                var cdCnt: Long = 1
+                while (true) {
+                    if (++abIdx == size) break
+                    if (abSumList[abIdx - 1] == abSumList[abIdx]) {
+                        abCnt++
+                    } else break
+                }
+
+                while (true) {
+                    if (++cdIdx == size) break
+                    if (cdSumList[cdIdx - 1] == cdSumList[cdIdx]) {
+                        cdCnt++
+                    } else break
+                }
+                result += (abCnt * cdCnt)
+            }
+            sumVal > 0 -> {
+                cdIdx++
+            }
+            else -> {
+                abIdx++
+            }
+        }
+    }
+
+    println(result)
 
 }
