dd8c883d5dba7080c04ccd90e408bc8d4431132d
juneyoung-jo@github.com <juneyoung-jo@github.com>
2023-12-27 23:40:26 +0900

    feat: 달리기경주 - 1
    - 프로그래머스

---
+++ src/programmers/달리기경주.java
@@ -0,0 +1,18 @@
+package programmers;
+
+import java.util.HashMap;
+
+public class 달리기경주 {
+
+    public String[] solution(String[] players, String[] callings) {
+        String[] answer = {};
+        HashMap<Integer, String> order = new HashMap<>();
+        for (int i = 0; i < players.length; i++) {
+            order.put(i, players[i]);
+        }
+
+
+        return answer;
+    }
+
+}
