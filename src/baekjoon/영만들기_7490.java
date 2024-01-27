92a6f4fb10fdf648c9927f86bb4bc72961cccb70
juneyoung-jo@github.com <juneyoung-jo@github.com>
2024-01-28 03:29:42 +0900

    feat: solved 0 만들기_7490
    - 백준
    - dfs, 구현

--- src/baekjoon/영만들기_7490.java
+++ src/baekjoon/영만들기_7490.java
@@ -1,8 +1,57 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class 영만들기_7490 {
 
-    public static void main(String[] args) {
+    private static char[] signs = {' ', '+', '-'};
+    private static StringBuilder sb = new StringBuilder();
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            dfs(1, n, "");
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void dfs(int start, int end, String acc) {
+        acc += start;
+        if (start == end) {
+            if(isZero(acc.replace(" ", ""))){
+                sb.append(acc).append("\n");
+            }
+            return;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            acc += signs[i];
+            dfs(start + 1, end, acc);
+            acc = acc.substring(0, acc.length() - 1);
+        }
+    }
+
+    private static boolean isZero(String expression) {
+        String[] operands = expression.split("[\\+\\-]");
+        String[] operators = expression.split("[0-9]+");
+
+        int result = Integer.parseInt(operands[0]);
+
+        for (int i = 1; i < operands.length; i++) {
+            int operand = Integer.parseInt(operands[i]);
+            if (operators[i].equals("+")) {
+                result += operand;
+            } else if (operators[i].equals("-")) {
+                result -= operand;
+            }
+        }
+
+        return result == 0;
     }
 }
